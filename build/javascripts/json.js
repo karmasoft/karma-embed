24a2dc81ff46c374bd1aa6ca9c8df04a7a0621e4
Dima Berastau <dima.berastau@gmail.com>
2021-02-15 21:49:23 +0100

    Updating links.

--- build/javascripts/json.js
+++ build/javascripts/json.js
@@ -1,32 +1,32 @@
 $(function() {
     var console_style;
     console_style = 'background: #1a901e;border-radius: 3px;color: #fff;';
-    $.getJSON("https://qa1.karmasoft.io"  + "/schedule.json", function(data) {
+    $.getJSON("demo.karmasoft.io"  + "/schedule.json", function(data) {
         var schedule;
         console.log('%c Schedule: ', console_style);
         console.log(data);
-        schedule = "<td>" + data.dates[1].day_name + " / " + data.dates[1].wday + " / " + data.dates[1].long + "</td>";
+        schedule = "<td>" + data.dates[0].day_name + " / " + data.dates[0].wday + " / " + data.dates[0].long + "</td>";
         return $('#schedule').replaceWith(schedule);
     });
-    $.getJSON("https://qa1.karmasoft.io"  + "/member/workshops.json", function(data) {
+    $.getJSON("demo.karmasoft.io"  + "/member/workshops.json", function(data) {
         var workshops;
         console.log('%c Workshops: ', console_style);
         console.log(data);
-        workshops = "<td>" + data[1].name + " / " + data[1].start_date + " / " + data[1].timetable + "</td>";
+        workshops = "<td>" + data[0].name + " / " + data[0].start_date + " / " + data[0].timetable + "</td>";
         return $('#workshops').replaceWith(workshops);
     });
-    $.getJSON("https://qa1.karmasoft.io"  + "/users/profiles.json", function(data) {
+    $.getJSON("demo.karmasoft.io"  + "/users/profiles.json", function(data) {
         var instructors;
         console.log('%c Instructors: ', console_style);
         console.log(data);
-        instructors = "<td>" + data[1].name + " / " + data[1].schedule_url + "</td>";
+        instructors = "<td>" + data[0].name + " / " + data[0].schedule_url + "</td>";
         return $('#instructors').replaceWith(instructors);
     });
-    return $.getJSON("https://qa1.karmasoft.io"  + "/member/store.json", function(data) {
+    return $.getJSON("demo.karmasoft.io"  + "/member/store.json", function(data) {
         var store;
         console.log('%c Store: ', console_style);
         console.log(data);
-        store = "<td>" + data[1].name + " / " + data[1].tax + " / " + data[1].type + "</td>";
+        store = "<td>" + data[0].name + " / " + data[0].tax + " / " + data[0].type + "</td>";
         return $('#store').replaceWith(store);
     });
 });
